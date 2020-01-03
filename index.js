bc8b79eda8ccf46515d977586dc29d5e9399a1b0
Mohammed Noori <kmanoorib@gmail.com>
2020-01-03 04:02:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const findMatching = function(drivers, name) {
 
 const fuzzyMatch = function(drivers, str) {
   length = str.length;
+  return drivers.filter(driver => {
+    return driver.slice(0, length) === str;
+  })
+  
 }
 
 const matchName = function(drivers, name) {
@@ -15,3 +19,8 @@ const matchName = function(drivers, name) {
     return driver.name.toLowerCase() === name.toLowerCase();
   })
 }
+
+
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
